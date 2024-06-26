4f43728b47a35733df61cca363309fee14334d88
pardhu4321 <154002982+pardhu4321@users.noreply.github.com>
2024-06-26 21:52:53 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -17,8 +17,8 @@ class Var(object):
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002048542710'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", ""))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1278741735").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'DPS_Movies'))
